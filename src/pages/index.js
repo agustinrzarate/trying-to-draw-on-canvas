bf92f03b6cffae6c1c2017ebd503a458d4609d47
Agustin R. Zarate <agustinrzarate@gmail.com>
2022-09-03 19:32:49 -0300

    Improving the design and animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,27 @@ import { useEffect, useRef, useState } from 'react'
 
 const Home = (props) => {
   const canvasRef = useRef(null)
+  const mousePosition = {
+    x: undefined,
+    y: undefined
+  }
+  const colors = ['#127369', '#BFBFBF', '#8AA6A3', '#4C5958']
   const maxRadius = 35
 
-  function Circle (x, y, dx, dy, radius, ctx) {
+  const [screen, setScreen] = useState(null)
+
+  function Circle (x, y, dx, dy, radius, ctx, fillColor) {
     this.x = x
     this.y = y
     this.dx = dx
     this.dy = dy
     this.radius = radius
-
+    this.fillColor = fillColor
+    const originalRadius = this.radius
     this.draw = function () {
       ctx.beginPath()
-      ctx.strokeStyle = '#7FB77E'
       ctx.arc(x, y, radius, 2 * Math.PI, false)
-      ctx.strokeStyle = 'blue'
-      ctx.stroke()
+      ctx.fillStyle = fillColor
       ctx.fill()
     }
 
@@ -31,11 +37,33 @@ const Home = (props) => {
       x = x + dx
       y = y + dy
 
+      const distanceX = x - mousePosition.x
+      const distanceY = y - mousePosition.y
+      const distance = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2))
+      if (distance < 50 && radius < maxRadius) {
+        radius = radius + 1
+      } else if (distance > 50 && radius > originalRadius && radius > 2) {
+        radius = radius - 1
+      }
+      if (!mousePosition.x && radius !== originalRadius && radius > 2) {
+        radius = radius - 1
+      }
       this.draw()
     }
   }
 
+  const handleMouseMove = (event) => {
+    mousePosition.x = event.clientX
+    mousePosition.y = event.clientY
+  }
+
+  const handleMouseLeave = () => {
+    mousePosition.x = undefined
+    mousePosition.y = undefined
+  }
+
   useEffect(() => {
+    console.log('ref', canvasRef)
     const canvas = canvasRef.current
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
@@ -43,14 +71,13 @@ const Home = (props) => {
     let animationFrameId
     const array = []
     for (let cant = 0; cant < 600; cant++) {
-      const radius = 3
+      const radius = Math.random() * 5
       const x = Math.random() * (innerWidth - radius * 2) + radius
       const y = Math.random() * (innerHeight - radius * 2) + radius
-
       const dx = Math.random() - 0.5
       const dy = Math.random() - 0.5
-
-      array.push(new Circle(x, y, dx, dy, radius, context))
+      const fillColor = colors[Math.floor(Math.random() * 4)]
+      array.push(new Circle(x, y, dx, dy, radius, context, fillColor))
     }
 
     const render = () => {
@@ -67,15 +94,15 @@ const Home = (props) => {
   }, [])
 
   return (
-    <canvas
-      ref={canvasRef}
-      {...props}
-
-      style={{
-        width: '100%',
-        height: '100%'
-      }}
-    />
+    <div>
+      <canvas
+        style={{ background: '#10403B', overflow: 'hidden' }}
+         onMouseLeave={handleMouseLeave}
+         onMouseMove={handleMouseMove}
+        ref={canvasRef}
+        {...props}
+      />
+    </div>
   )
 }
 
